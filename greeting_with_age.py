f27ce106e9a8ca24b9b66b6b332fd41f4b216379
Oxidised <163737148+wh3mao@users.noreply.github.com>
2025-04-04 19:50:23 +0300

    Add files via upload

---
+++ greeting_with_age.py
@@ -0,0 +1,11 @@
+print("Hello, enter your name")
+user_name = input()
+print("Enter your age")
+user_age = int(input())
+
+if user_age < 18:
+    print(f"{user_name} you are young")
+else:
+ print(f"Hello, {user_name}, your age is {user_age}")
+
+
